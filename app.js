be5d9b5eeed169bc053b69e73f89797770026fd1
Abdulrahman Elnabwi <abdulrahman.elnabwi@gmail.com>
2022-09-01 21:27:12 +0200

    update 1

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var navbar = document.querySelector('nav');
 
 navbar.addEventListener('click', function(e){
     window.location.hash= e.target.id+'_';
-    document.querySelectorAll('li').forEach(e =>{
+    document.querySelectorAll('navbar li').forEach(e =>{
         if(e.classList.contains('active')){
             e.classList.remove('active');
         }
     })
-    e.target.classList.add('active');
     onScrolling();
 });
 
@@ -25,16 +24,20 @@ window.onscroll = function(e){
     onScrolling();
 }
 
-document.addEventListener('click', function(event){
-    var span = document.querySelector(`span[data-key='${event.target.getAttribute('data-key')}']`);
-    var sec  =document.querySelector(`section[data-key='${event.target.getAttribute('data-key')}']`);
-    if(span.classList.contains('collapes')){
-        span.classList.remove('collapes');
-        sec.classList.remove('sectionCollapedHeight');
-        
-    }
-    else{
-        span.classList.add('collapes');
-        sec.classList.add('sectionCollapedHeight');
+var is = document.querySelectorAll('i');
+
+is.forEach(function(e){
+    e.onclick = () =>{
+        var span = document.querySelector(`span[data-key='${event.target.getAttribute('data-key')}']`);
+        var sec  =document.querySelector(`section[data-key='${event.target.getAttribute('data-key')}']`);
+        if(span.classList.contains('collapes')){
+            span.classList.remove('collapes');
+            sec.classList.remove('sectionCollapedHeight');
+            
+        }
+        else{
+            span.classList.add('collapes');
+            sec.classList.add('sectionCollapedHeight');
+        }
     }
 })
