c208cf7a60fe74190600dd8be4d75165951ab8b3
Abdelrhman Elnabawy <abdulrahman.elnabwi@gmail.com>
2022-09-08 10:46:44 +0200

    I hope it's the last update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 var navbar = document.querySelector('nav');
-const sections = document.querySelectorAll('section');
-
+var sections = document.querySelectorAll('section');
 
 // this to create the navber elment
 var ul = document.createElement('ul');
+
 for(let i =1; i<= sections.length; i++){
-    const li = document.createElement('li')
+    var li = document.createElement('li')
+    li.setAttribute('data-key', `${i}`)
     li.setAttribute('id',`section${i}`);
-    const a = document.createElement('a');
-    a.href = `#section${i}_`;
-    a.textContent = `Section ${i}`;
-    li.appendChild(a);
+    li.textContent = `Section${i}`;
     ul.appendChild(li);
 }
+
 navbar.appendChild(ul);
 
+// this fucntion execpt to to take an iterators and class which you want to remove from the iterator
+function removeClass(iterators,className){
+    iterators.forEach(e=>{
+        if(e.classList.contains(className)){
+            e.classList.remove(className);
+        }
+    })
+}
 
+var lists = document.querySelectorAll('li');
+ul.onclick = (e) =>{
+    removeClass(lists,'active');
+    e.target.classList.add('active');
+    window.location.hash = e.target.id+'_';
+}
 
 // this function to show navbar which you scroll or click and do that using visiibilty
 function onScrolling(){
@@ -26,15 +39,14 @@ function onScrolling(){
     },5000)
 }
 
-
 // this elment in which by click on will hide the detail of a section
 var is = document.querySelectorAll('i');
-
 is.forEach(function(e){
     e.onclick = () =>{
-        let span = document.querySelector(`span[data-key='${event.target.getAttribute('data-key')}']`);
-        let sec  =document.querySelector(`section[data-key='${event.target.getAttribute('data-key')}']`);
+        var span = document.querySelector(`span[data-key='${event.target.getAttribute('data-key')}']`);
+        var sec  =document.querySelector(`section[data-key='${event.target.getAttribute('data-key')}']`);
         // it use the css class collapes to hide and to give it a less hight using  sectionCollapedHeight css class
+
         if(span.classList.contains('collapes')){
             span.classList.remove('collapes');
             sec.classList.remove('sectionCollapedHeight');
@@ -47,52 +59,27 @@ is.forEach(function(e){
     }
 })
 
-
-// this code to control the active li in the ul
-var ul = document.querySelector('ul');
-const childs = ul.children;
-
 // this code control the active section , the active list and
 window.onscroll = (e) =>{
-    onScrolling();
-    e.preventDefault();
     const element = document.body.getBoundingClientRect();
-    const se = document.querySelector('section');
+    const se = document.querySelector('section').getBoundingClientRect().height;
     // find which section is on views
-    let postion = Math.round(-((element.top)/ se.getBoundingClientRect().height));
-    let sectionName = 'section'+postion;
+    const postion = Math.round(-((element.top)/ se));
     // find the section name
-    let section = 'section'+postion+'_';
-    let div = document.querySelector(`div[data-key="${1}"]`);
-    for(let i = 0; i <sections.length; i++){
-        div = document.querySelector(`div[data-key="${i+1}"]`);
-        // this to style the active section 
-        if(sections[i].id === section){
-            sections[i].classList.add('active-class');
-        }
-        else if(sections[i].classList.contains('active-class')){
-            sections[i].classList.remove('active-class');
-        }
+    let sectionId = 'section'+postion+'_';
+    removeClass(sections, 'active-class');
+    removeClass(sections,'circle');
 
-        //this to remove the circle if it viewd in any sections
-        if(div.classList.contains('circle')){
-            div.classList.remove('circle');
-        }
-
-        // give the active section in the
-        if(childs[i].id === sectionName){
-            childs[i].classList.add('active');
-            childs[i].children[0].style.color = 'white';
-        }
-        else if(childs[i].classList.contains('active')){
-            childs[i].classList.remove('active');
-            childs[i].children[0].style.color = 'black';
-        }
-    }
     // makes the active section view the circle
+    // add the active to the perticuler elemnt and add
     if(postion >= 1){
-        div = document.querySelector(`div[data-key="${postion}"`);
+        var lis= document.querySelector(`li[data-key="${postion}"]`);
+        removeClass(lists,'active');      
+        lis.classList.add('active');
+        var section = document.querySelector(`#${sectionId}`);
+        var div = document.querySelector(`div[data-key="${postion}"]`);
+        section.classList.add('active-class');
         div.classList.add('circle');
     }
-
+    onScrolling();
 }
